ccb604a64fbe2f5f544b01e3a1d7b5b83f865348
Fabio Mattes <fabio.mattes@benner.com.br>
2021-07-04 17:19:53 -0300

    040720211719

--- Backend/src/ProAgil.Persistence/IProAgilPersistence.cs
+++ Backend/src/ProAgil.Persistence/IProAgilPersistence.cs
@@ -9,20 +9,20 @@ namespace ProAgil.Persistence
         void Add<T>(T entity) where T : class;
         void Update<T>(T entity) where T : class;
         void Delete<T>(T entity) where T : class;
-        void DeleteRange<T>(T[] entity) where T : class;
+        void DeleteRange<T>(T[] entityArray) where T : class;
         Task<bool> SaveChangesAsync();
         #endregion
 
         #region EVENTOS
         Task<Evento[]> GetAllEventosByTemaAsync(string tema, bool includePalestrantes);
         Task<Evento[]> GetAllEventosAsync(bool includePalestrantes);
-        Task<Evento> GetAllEventoByIdAsync(int EventoId, bool includePalestrantes);
+        Task<Evento> GetEventoByIdAsync(int EventoId, bool includePalestrantes);
         #endregion
 
         #region PALESTRANTES
         Task<Palestrante[]> GetAllPalestrantesByNomeAsync(string nome, bool includeEventos);
         Task<Palestrante[]> GetAllPalestrantesAsync(bool includeEventos);
-        Task<Palestrante> GetAllPalestranteByIdAsync(int PalestranteId, bool includeEventos);
+        Task<Palestrante> GetPalestranteByIdAsync(int PalestranteId, bool includeEventos);
         #endregion
     }
 }
