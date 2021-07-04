6b637aebf49c72613daadef96474dd8da9180791
Fabio Mattes <fabio.mattes@benner.com.br>
2021-07-04 13:06:05 -0300

    040720211306

--- Frontend/ProAgil-App/src/app/app.module.ts
+++ Frontend/ProAgil-App/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -26,6 +27,7 @@ import { TooltipModule } from 'ngx-bootstrap/tooltip';
     BrowserAnimationsModule,
     CollapseModule.forRoot(),
     TooltipModule.forRoot(),
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- Frontend/ProAgil-App/src/app/eventos/eventos.component.ts
+++ Frontend/ProAgil-App/src/app/eventos/eventos.component.ts
@@ -8,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
   public eventos: any = [];
+  public eventosFiltrados: any = [];
+
   larguraImg: number = 150;
   margemImg: number = 2;
   mostrarImg: boolean = false;
+
+  private _filtroLista: string = '';
+
+  public get filtroLista(): string {
+    return this._filtroLista;
+  }
+
+  public set filtroLista(value: string) {
+    this._filtroLista = value;
+    this.eventosFiltrados = this.filtroLista
+      ? this.filtrarEventos(this.filtroLista)
+      : this.eventosFiltrados;
+  }
 
   constructor(private http: HttpClient) {}
 
@@ -20,12 +35,24 @@ export class EventosComponent implements OnInit {
 
   public getEventos(): void {
     this.http.get('https://localhost:5001/api/Evento').subscribe(
-      (response) => (this.eventos = response),
+      (response) => {
+        this.eventos = response;
+        this.eventosFiltrados = response;
+      },
       (error) => console.log(error)
     );
   }
 
   public mostrarImagem(): boolean {
     return (this.mostrarImg = !this.mostrarImg);
+  }
+
+  filtrarEventos(filtrarPor: string): any {
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter(
+      (evento: any) =>
+        evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
+        evento.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    );
   }
 }
